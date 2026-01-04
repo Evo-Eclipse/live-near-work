51adabebd247966f94ff4cf30badd1157784f155
Evo Eclipse <134649506+Evo-Eclipse@users.noreply.github.com>
2026-01-04 16:55:05 +0300

    chore(metadata): add preview image and update mod description

--- metadata.lua
+++ metadata.lua
@@ -6,4 +6,11 @@ return PlaceObj('ModDef', {
 	'lua_revision', 350453,
 	'saved_with_revision', 384011,
 	'saved', 1767429520,
+	'description', "Keeps your colonists happy by relocating them closer to work, preventing morale penalties from inter-dome commutes and improving colony efficiency.",
+	'image', "Preview.jpeg",
+	'code', {
+		"Code/Script.lua",
+	},
+	'TagGameplay', true,
+	'TagOther', true,
 })

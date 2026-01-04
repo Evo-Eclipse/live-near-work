45fee3c8136692268b9402c76b88fd5e68e23c9d
Evo Eclipse <134649506+Evo-Eclipse@users.noreply.github.com>
2026-01-04 08:39:44 +0300

    Initial Commit

---
+++ metadata.lua
@@ -0,0 +1,9 @@
+return PlaceObj('ModDef', {
+	'title', "live-near-work",
+	'id', "cUmUwA",
+	'author', "Evo 1",
+	'version', 1,
+	'lua_revision', 350453,
+	'saved_with_revision', 384011,
+	'saved', 1767429520,
+})
